c1dee95d6e4e1307dc3cc3f49c6defa846e4bc62
santiago pedraza <103968703+SantiagoPedraza@users.noreply.github.com>
2024-07-11 20:14:52 -0300

    bdd

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'expense_management',
-    password: 'SANti11.11',
+    user: 'gastos',
+    host: 'dpg-cq86dnss1f4s73cg6fbg-a',
+    database: 'expense_management_qn0p',
+    password: 'rjxcvzrFlj4ol4HfwAw89YnKeAN86kLB',
     port: 5432,
 });
 
